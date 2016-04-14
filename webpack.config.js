8aca91d3ed0af3ec33de2f124108bcf12a45983c
AntonYaskevich <anton.yaskevich.mail@gmail.com>
2016-04-14 18:11:53 +0300

    lu: client.initial-dev-config.webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+'use strict'
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        auth: path.resolve(__dirname, 'src/renders/auth/index.js'),
+        app: path.resolve(__dirname, 'src/renders/app/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devtool: 'cheap-module-source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js[x]?$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    plugins: [
+                        'transform-runtime',
+                        'transform-decorators-legacy',
+                    ],
+                    presets: ['es2015', 'react', 'stage-0']
+                }
+            },
+            {
+                test: /\.html$/,
+                loader: "file?name=[name].[ext]"
+            }
+        ]
+    }
+}
